91a94da4b668dc994f950ee2ffa1795559106974
Praveen Laxkar <laxkardalauda1@gmail.com>
2026-08-03 20:40:29 +0530

    fix: export SmartBusinessContractLegalDeedStudioPage for appLoaders contract

--- src/apps/smart-legal-deed-studio/index.tsx
+++ src/apps/smart-legal-deed-studio/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import SmartLegalDeedStudioPage from './SmartLegalDeedStudioPage';
 
-export const SmartLegalDeedStudioExport: React.FC = () => {
+export const SmartBusinessContractLegalDeedStudioPage: React.FC = () => {
   return <SmartLegalDeedStudioPage />;
 };
 
-export { SmartLegalDeedStudioPage as SmartLegalDeedStudioPageNamed };
+export { SmartLegalDeedStudioPage };
 export default SmartLegalDeedStudioPage;
