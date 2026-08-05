7fdb13d47946fe24a10ebe7de84f7f7daef1744b
Praveen Laxkar <laxkardalauda1@gmail.com>
2026-08-05 08:29:23 +0530

    resolved

--- src/core/apps/appLoaders.ts
+++ src/core/apps/appLoaders.ts
@@ -382,7 +382,7 @@ export const APP_LOADERS: AppLoaderDefinition[] = [
     name: "Zip2Text Studio",
     component: lazy(() =>
       import("@apps/zip2text-studio").then((module) => ({
-        default: module.default || module.Zip2TextStudioPage,
+      default: module.default || module.Zip2TextStudioApp,
       }))
     ),
   }
